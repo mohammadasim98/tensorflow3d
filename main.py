15438a7ba299a630fd477a89b402200e34f7cf1d
Mohammad Asim <asim.98.12.26@gmail.com>
2022-11-30 15:00:45 +0100

    fixed point cloud ascii test, modified point cloud base class

--- main.py
+++ main.py
@@ -10,12 +10,8 @@
 """
 
 import tensorflow3d as t3d
-import numpy as np
-import polyscope as ps
 
 if __name__ == '__main__':
-    ps.init()
-    points = np.random.rand(100,3)
     pcd = t3d.representation.PointCloud()
-    pcd.load("D:/research/tensorflow3d/tensorflow3d/tests/formats/ascii/xyz.txt")
+    pcd.load("D:/research/tensorflow3d/tensorflow3d/tests/formats/ascii/xyz.txt", format='xyzi')
     pcd.render()
